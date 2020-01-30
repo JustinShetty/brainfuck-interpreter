a9e8b40025ee0f233965e9ab251abe4d4eba3c4e
Justin Shetty <jas0504@gmail.com>
2020-01-30 01:44:45 -0800

    Loop and I/O operations implemented. README expanded.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # brainfuck-interpreter
 A Python3 [Brainfuck](https://en.wikipedia.org/wiki/Brainfuck) interpreter
+
+Usage: `./brainfuck.py [-h] input_filename`
+
+The contents of `sample_input.bf` are taken from the Wikipedia article linked above.
--- brainfuck.py
+++ brainfuck.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+#
+# Author: Justin Shetty
+# Date:   30 January 2020
+#
 import sys
 import argparse
 
@@ -8,49 +12,55 @@ class Brainfuck:
 
     def __init__(self):
         self.memory_ = [0]
-        self.memory_len_ = 1
-        self.data_ptr_ = 0
+        self.mem_len_ = 1
+        self.mem_idx_ = 0
+        self.loop_pos_ = []
 
     def run(self, filename):
         with open(filename, 'r') as f:
-            chars = [ch for line in f.readlines() for ch in line.rstrip().replace(' ', '')]
+            chars = [
+                ch for line in f.readlines()
+                for ch in line.rstrip().replace(' ', '')
+            ]
             ops = [ch for ch in chars if ch in self.VALID_OPS]
             self.evaluate(ops)
 
     def evaluate(self, ops):
-        for i in range(len(ops)):
-            op = ops[i]
+        op_idx = 0
+        while op_idx < len(ops):
+            op = ops[op_idx]
             if op == '>':
-                self.data_ptr_ += 1
-                if self.data_ptr_ == self.memory_len_:
-                    self.memory_len_ += 1
+                self.mem_idx_ += 1
+                if self.mem_idx_ == self.mem_len_:
+                    self.mem_len_ += 1
                     self.memory_.append(0)
             elif op == '<':
-                self.data_ptr_ -= 1
-                if self.data_ptr_ < 0:
-                    raise IndexError('Index -1 outside memory bounds')
+                self.mem_idx_ -= 1
+                if self.mem_idx_ < 0:
+                    raise IndexError('Index -1 outside bounds')
             elif op == '+':
-                self.memory_[self.data_ptr_] += 1
+                self.memory_[self.mem_idx_] += 1
             elif op == '-':
-                self.memory_[self.data_ptr_] -= 1
+                self.memory_[self.mem_idx_] -= 1
             elif op == '.':
-                sys.stdout.write(chr(self.memory_[self.data_ptr_]))
+                sys.stdout.write(chr(self.memory_[self.mem_idx_]))
             elif op == ',':
-                self.memory_[self.data_ptr_] = sys.stdin.read(1)
+                self.memory_[self.mem_idx_] = sys.stdin.read(1)
             elif op == '[':
-                pass
+                self.loop_pos_.append(op_idx)
             elif op == ']':
-                pass
-
-
-def main(args):
-    bf = Brainfuck()
-    bf.run(args.input_filename)
-    # print(bf.memory_)
+                if self.memory_[self.mem_idx_] == 0:
+                    self.loop_pos_.pop()
+                else:
+                    op_idx = self.loop_pos_[-1]
+            op_idx += 1
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Brainfuck Interpreter")
-    parser.add_argument('input_filename', type=str, help='ASCII-encoded source file')
+    parser.add_argument('input_filename',
+                        type=str,
+                        help='ASCII-encoded source file')
     args = parser.parse_args()
-    main(args)
+    bf = Brainfuck()
+    bf.run(args.input_filename)
