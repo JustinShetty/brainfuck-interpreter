ec0a1dac5a32bcff06b34391c5be7d208344bab1
Justin Shetty <jas0504@gmail.com>
2020-01-30 02:14:57 -0800

    Previous commit had incorrect loop behavior.

--- brainfuck.py
+++ brainfuck.py
@@ -14,21 +14,32 @@ class Brainfuck:
         self.memory_ = [0]
         self.mem_len_ = 1
         self.mem_idx_ = 0
-        self.loop_pos_ = []
 
     def run(self, filename):
-        with open(filename, 'r') as f:
-            chars = [
-                ch for line in f.readlines()
-                for ch in line.rstrip().replace(' ', '')
-            ]
-            ops = [ch for ch in chars if ch in self.VALID_OPS]
-            self.evaluate(ops)
+        opstring = self.parse_ops(filename)
+        self.evaluate(opstring)
 
-    def evaluate(self, ops):
+    def parse_ops(self, filename):
+        with open(filename, 'r') as f:
+            return ''.join([ch for ch in f.read() if ch in self.VALID_OPS])
+
+    def bracket_pairing(self, opstring):
+        brackets = {}
+        stack = []
+        for i, op in enumerate(opstring):
+            if op == '[':
+                stack.append(i)
+            elif op == ']':
+                opener = stack.pop()
+                brackets[opener] = i
+                brackets[i] = opener
+        return brackets
+
+    def evaluate(self, opstring):
+        brackets = self.bracket_pairing(opstring)
         op_idx = 0
-        while op_idx < len(ops):
-            op = ops[op_idx]
+        while op_idx < len(opstring):
+            op = opstring[op_idx]
             if op == '>':
                 self.mem_idx_ += 1
                 if self.mem_idx_ == self.mem_len_:
@@ -46,13 +57,10 @@ class Brainfuck:
                 sys.stdout.write(chr(self.memory_[self.mem_idx_]))
             elif op == ',':
                 self.memory_[self.mem_idx_] = sys.stdin.read(1)
-            elif op == '[':
-                self.loop_pos_.append(op_idx)
-            elif op == ']':
-                if self.memory_[self.mem_idx_] == 0:
-                    self.loop_pos_.pop()
-                else:
-                    op_idx = self.loop_pos_[-1]
+            elif op == '[' and self.memory_[self.mem_idx_] == 0:
+                op_idx = brackets[op_idx]
+            elif op == ']' and self.memory_[self.mem_idx_] != 0:
+                op_idx = brackets[op_idx]
             op_idx += 1
 
 
